93f9192e7e32d45e7e7a171e7afeda03d5fc9702
jonat <jonat@LAPTOP-ABBV6MT5>
2020-11-20 18:37:15 -0400

    botones en inst3 primera pagina

--- finalSoftware/src/Criterios_I3.java
+++ finalSoftware/src/Criterios_I3.java
@@ -8,18 +8,18 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Criterios_I3 extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_1;
 	private JTextField textField_2;
-	private JTextField textField_3;
 	private JTextField textField_4;
-	private JTextField textField_5;
 	private JTextField textField_6;
-	private JTextField textField_7;
 
 	/**
 	 * Launch the application.
@@ -73,11 +73,6 @@ public class Criterios_I3 extends JFrame {
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(736, 122, 96, 19);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
 		JLabel lblNewLabel_1_1 = new JLabel("2. Usa ejemplos o ejercicios relacionados con el tema de la clase.");
 		lblNewLabel_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblNewLabel_1_1.setBounds(27, 244, 382, 14);
@@ -92,11 +87,6 @@ public class Criterios_I3 extends JFrame {
 		textField_2.setColumns(10);
 		textField_2.setBounds(118, 289, 486, 76);
 		contentPane.add(textField_2);
-		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(736, 289, 96, 19);
-		contentPane.add(textField_3);
 		
 		JLabel lblNewLabel_3_1 = new JLabel("Puntuacion");
 		lblNewLabel_3_1.setFont(new Font("Arial", Font.PLAIN, 12));
@@ -123,11 +113,6 @@ public class Criterios_I3 extends JFrame {
 		lblNewLabel_3_1_1.setBounds(658, 450, 69, 13);
 		contentPane.add(lblNewLabel_3_1_1);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(736, 447, 96, 19);
-		contentPane.add(textField_5);
-		
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("4. Relaciona la utilidad del tema con otros cursos o areas del conocimiento.");
 		lblNewLabel_1_1_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblNewLabel_1_1_1_1.setBounds(27, 561, 446, 14);
@@ -148,13 +133,34 @@ public class Criterios_I3 extends JFrame {
 		lblNewLabel_3_1_1_1.setBounds(658, 612, 69, 13);
 		contentPane.add(lblNewLabel_3_1_1_1);
 		
-		textField_7 = new JTextField();
-		textField_7.setColumns(10);
-		textField_7.setBounds(736, 609, 96, 19);
-		contentPane.add(textField_7);
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerNumberModel(0, 0, 4, 1));
+		spinner.setBounds(736, 163, 30, 20);
+		contentPane.add(spinner);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setModel(new SpinnerNumberModel(0, 0, 4, 1));
+		spinner_1.setBounds(736, 317, 30, 20);
+		contentPane.add(spinner_1);
+		
+		JSpinner spinner_2 = new JSpinner();
+		spinner_2.setModel(new SpinnerNumberModel(0, 0, 4, 1));
+		spinner_2.setBounds(736, 478, 30, 20);
+		contentPane.add(spinner_2);
+		
+		JSpinner spinner_3 = new JSpinner();
+		spinner_3.setModel(new SpinnerNumberModel(0, 0, 4, 1));
+		spinner_3.setBounds(736, 637, 30, 20);
+		contentPane.add(spinner_3);
 		
 		JButton btnNewButton = new JButton("Continuar");
-		btnNewButton.setBounds(386, 734, 112, 21);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnNewButton.setBounds(325, 762, 101, 21);
 		contentPane.add(btnNewButton);
 	}
 }
