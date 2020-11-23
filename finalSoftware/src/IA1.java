f2f07d403eccc42563e0b385f19ce92d0817f8e3
jonat <jonat@LAPTOP-ABBV6MT5>
2020-11-23 01:17:19 -0400

    tab 3 I1A

--- finalSoftware/src/IA1.java
+++ finalSoftware/src/IA1.java
@@ -11,6 +11,7 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.awt.Color;
 
 public class IA1 {
 
@@ -70,8 +71,8 @@ public class IA1 {
 		panel.add(lblEnLosCursos);
 		
 		JLabel lblNewLabel_1 = new JLabel("PARTE I. Perfil del estudiante. Contestes las siguientes preguntas.");
-		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 12));
-		lblNewLabel_1.setBounds(72, 200, 415, 26);
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel_1.setBounds(72, 200, 493, 26);
 		panel.add(lblNewLabel_1);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Mas de 5 horas a la semana.");
@@ -163,7 +164,7 @@ public class IA1 {
 		spinner_1.setBounds(804, 132, 30, 20);
 		panel_1.add(spinner_1);
 		
-		JLabel lblNewLabel_1_1_2 = new JLabel("PARTE II. El prontuario del curso. Utiliza la calve para las preguntas 3 y 4:");
+		JLabel lblNewLabel_1_1_2 = new JLabel("PARTE III. Desarrollo de las clases en linea. Conteste las preguntas 5-13.");
 		lblNewLabel_1_1_2.setFont(new Font("Arial", Font.BOLD, 14));
 		lblNewLabel_1_1_2.setBounds(10, 194, 528, 26);
 		panel_1.add(lblNewLabel_1_1_2);
@@ -276,12 +277,160 @@ public class IA1 {
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("New tab", null, panel_2, null);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1_1_2_1 = new JLabel("PARTE IV. Gerencia del procesos de ense\u00F1anza aprendizaje. Conteste las");
+		lblNewLabel_1_1_2_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_1_1_2_1.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel_1_1_2_1.setBounds(10, 10, 528, 26);
+		panel_2.add(lblNewLabel_1_1_2_1);
+		
+		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("(4) Totalmente de acuerdo (si)");
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_1_1_1_1_1.setBounds(10, 75, 477, 14);
+		panel_2.add(lblNewLabel_1_1_1_1_1);
+		
+		JLabel lblNewLabel_1_1_1_2_1 = new JLabel("(3) Parcialmente de acuerdo(mayormente si)");
+		lblNewLabel_1_1_1_2_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_1_1_1_2_1.setBounds(10, 99, 477, 14);
+		panel_2.add(lblNewLabel_1_1_1_2_1);
+		
+		JLabel lblNewLabel_1_1_1_3_1 = new JLabel("(2) Parcialmente en desacuerdo (mayormente no)");
+		lblNewLabel_1_1_1_3_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_1_1_1_3_1.setBounds(10, 123, 477, 14);
+		panel_2.add(lblNewLabel_1_1_1_3_1);
+		
+		JLabel lblNewLabel_1_1_1_4_1 = new JLabel("(1) Totalmente en desacuerdo (no).");
+		lblNewLabel_1_1_1_4_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_1_1_1_4_1.setBounds(10, 147, 477, 14);
+		panel_2.add(lblNewLabel_1_1_1_4_1);
+		
+		JLabel lblNewLabel_1_1_1_5_1 = new JLabel("(N/A) No aplica");
+		lblNewLabel_1_1_1_5_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_1_1_1_5_1.setBounds(10, 171, 477, 14);
+		panel_2.add(lblNewLabel_1_1_1_5_1);
+		
+		JLabel lblElProfesor_1_2 = new JLabel("14. El profesor aclara mis dudas sobre la estructura y organizacion del curso.");
+		lblElProfesor_1_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2.setBounds(10, 213, 528, 31);
+		panel_2.add(lblElProfesor_1_2);
+		
+		JLabel lblNewLabel_1_1_2_1_1 = new JLabel("Preguntas 14-24 tomando en consideracion la siguente premisa.");
+		lblNewLabel_1_1_2_1_1.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel_1_1_2_1_1.setBounds(10, 34, 528, 26);
+		panel_2.add(lblNewLabel_1_1_2_1_1);
+		
+		JLabel lblElProfesor_1_2_1 = new JLabel("15. El prodesor esta disponible para contestar mis preguntas en el horario anunciado.");
+		lblElProfesor_1_2_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1.setBounds(10, 254, 528, 31);
+		panel_2.add(lblElProfesor_1_2_1);
+		
+		JLabel lblElProfesor_1_2_1_1 = new JLabel("16. Los examenes y trabajos responden al contenido del curso.");
+		lblElProfesor_1_2_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1.setBounds(10, 295, 528, 31);
+		panel_2.add(lblElProfesor_1_2_1_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1 = new JLabel("17. El profesor utiliza dirveso metodos para evaluar el aprendizaje.");
+		lblElProfesor_1_2_1_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1.setBounds(10, 336, 528, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_1 = new JLabel("18. El profesor corrige los ex\u00E1menes y trabajos, seg\u00FAn los criterios establecidos en el prontuario.");
+		lblElProfesor_1_2_1_1_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_1.setBounds(10, 377, 553, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_1_1 = new JLabel("19. El profesor informa los resultados de los ex\u00E1menes y\r\ntrabajos corregidos, preferiblemente en o antes de\r\ndos semanas.");
+		lblElProfesor_1_2_1_1_1_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_1_1.setBounds(10, 418, 681, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_1_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_1_2 = new JLabel("20.  El profesor discute los resultados de mis\r\nevaluaciones, en su defecto, hace anotaciones o\r\ncomentarios en los espacios ");
+		lblElProfesor_1_2_1_1_1_1_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_1_2.setBounds(10, 459, 681, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_1_2);
+		
+		JLabel lblElProfesor_1_2_1_1_1_2 = new JLabel("provistos en los diferentes tipos de actividades.");
+		lblElProfesor_1_2_1_1_1_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_2.setBounds(35, 482, 528, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_2);
+		
+		JLabel lblElProfesor_1_2_1_1_1_1_1_1 = new JLabel("21.  El profesor exhorta a los estudiantes a comunicarse\r\ncon \u00E9l, en caso de que surjan dudas relacionadas con los");
+		lblElProfesor_1_2_1_1_1_1_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_1_1_1.setBounds(10, 523, 681, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_1_1_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_2_1 = new JLabel("temas del curso y sus evaluaciones.");
+		lblElProfesor_1_2_1_1_1_2_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_2_1.setBounds(35, 544, 528, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_2_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_1_2_1 = new JLabel("22. El profesor provee oportunidades flexibles de interacci\u00F3n entre estudiante-estudiante y estudiante");
+		lblElProfesor_1_2_1_1_1_1_2_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_1_2_1.setBounds(10, 585, 681, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_1_2_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_2_1_1 = new JLabel("profesor, usando los foros de discusi\u00F3n, los salones virtuales, entre otros.");
+		lblElProfesor_1_2_1_1_1_2_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_2_1_1.setBounds(35, 606, 528, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_2_1_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_1_1_1_1 = new JLabel("23. El profesor actualiza el plan de trabajo en el\r\ncalendario de actividades.\r");
+		lblElProfesor_1_2_1_1_1_1_1_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_1_1_1_1.setBounds(10, 647, 681, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_1_1_1_1);
+		
+		JLabel lblElProfesor_1_2_1_1_1_1_1_1_2 = new JLabel("24. El profesor procura que las diversas actividades del\r\ncurso se cumplan dentro del plazo establecido.");
+		lblElProfesor_1_2_1_1_1_1_1_1_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblElProfesor_1_2_1_1_1_1_1_1_2.setBounds(10, 688, 681, 31);
+		panel_2.add(lblElProfesor_1_2_1_1_1_1_1_1_2);
+		
+		JSpinner spinner_3 = new JSpinner();
+		spinner_3.setBounds(776, 219, 30, 20);
+		panel_2.add(spinner_3);
+		
+		JSpinner spinner_3_1 = new JSpinner();
+		spinner_3_1.setBounds(776, 260, 30, 20);
+		panel_2.add(spinner_3_1);
+		
+		JSpinner spinner_3_2 = new JSpinner();
+		spinner_3_2.setBounds(776, 301, 30, 20);
+		panel_2.add(spinner_3_2);
+		
+		JSpinner spinner_3_3 = new JSpinner();
+		spinner_3_3.setBounds(776, 342, 30, 20);
+		panel_2.add(spinner_3_3);
+		
+		JSpinner spinner_3_4 = new JSpinner();
+		spinner_3_4.setBounds(776, 383, 30, 20);
+		panel_2.add(spinner_3_4);
+		
+		JSpinner spinner_3_5 = new JSpinner();
+		spinner_3_5.setBounds(776, 424, 30, 20);
+		panel_2.add(spinner_3_5);
+		
+		JSpinner spinner_3_6 = new JSpinner();
+		spinner_3_6.setBounds(776, 465, 30, 20);
+		panel_2.add(spinner_3_6);
+		
+		JSpinner spinner_3_7 = new JSpinner();
+		spinner_3_7.setBounds(776, 529, 30, 20);
+		panel_2.add(spinner_3_7);
+		
+		JSpinner spinner_3_8 = new JSpinner();
+		spinner_3_8.setBounds(776, 591, 30, 20);
+		panel_2.add(spinner_3_8);
+		
+		JSpinner spinner_3_9 = new JSpinner();
+		spinner_3_9.setBounds(776, 653, 30, 20);
+		panel_2.add(spinner_3_9);
+		
+		JSpinner spinner_3_10 = new JSpinner();
+		spinner_3_10.setBounds(776, 694, 30, 20);
+		panel_2.add(spinner_3_10);
 		
 		JPanel panel_3 = new JPanel();
 		tabbedPane.addTab("New tab", null, panel_3, null);
 		panel_3.setLayout(null);
-		
-		JPanel panel_4 = new JPanel();
-		tabbedPane.addTab("New tab", null, panel_4, null);
 	}
 }
