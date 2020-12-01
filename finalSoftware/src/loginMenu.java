8686299788a8df47c5798d35e1e2d1b955bb6e67
ricar <ricar@DESKTOP-HVNDFHE>
2020-12-01 15:01:03 -0400

    Login y text file

---
+++ finalSoftware/src/loginMenu.java
@@ -0,0 +1,140 @@
+
+
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+
+
+
+	public class loginMenu implements ActionListener {
+		String username;
+		String password;
+		
+		
+
+		public static void main(String[] args) {
+			
+			
+			JPanel panel = new JPanel();
+			JFrame frame = new JFrame();
+			frame.setSize(350,200);
+			//frame.setBounds(100, 100, 932, 820);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			frame.getContentPane().add(panel);
+			
+			panel.setLayout(null);
+			
+			JLabel userLabel = new JLabel("Username");
+			userLabel.setBounds(10, 20, 80, 25);
+			panel.add(userLabel);
+			
+			JTextField userText = new JTextField(20);
+			userText.setBounds(100, 20, 165, 25);
+			panel.add(userText);
+			
+			JLabel passwordLabel = new JLabel("Password");
+			passwordLabel.setBounds(10, 50, 80, 25);
+			panel.add(passwordLabel);
+			
+			JTextField passwordText = new JTextField(20);
+			passwordText.setBounds(100, 50, 165, 25); 
+			panel.add(passwordText);
+			
+			JButton button= new JButton("Login");
+			button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					VerifyLogin();
+				}
+			});
+			button.setBounds(10, 80, 80, 25);
+			panel.add(button);
+			
+			JLabel success = new JLabel("");
+			success.setBounds(10,110,300,25);
+			//button.addActionListener(VerifyLogin());
+			panel.add(success);
+			//success.setText();
+			
+			frame.setVisible(true);
+			
+			
+			
+			
+			
+			//Class Execution
+			VerifyLogin();
+			
+		
+	}
+
+
+
+
+	public static boolean VerifyLogin() {
+			boolean isAuthenticated = false;
+			
+			
+			Scanner input = new Scanner(System.in);
+			System.out.println("What is your username: ");
+			String username = input.nextLine();
+			System.out.println("What is your password: ");
+			String password = input.nextLine();
+		
+		
+			String pathway = "C:\\Users\\ricar\\git\\finalsoftware\\finalSoftware\\src\\login.txt"; 
+			File file = new File(pathway);
+			
+				try {
+					@SuppressWarnings("resource")
+					Scanner inputBuffer = new Scanner(file);
+						
+					while(inputBuffer.hasNext()) {
+						String line = inputBuffer.nextLine();
+						String [] values = line.split("\t");
+						
+							if(values[0].equals(username)) {
+								if(values[1].equals(password)) {
+									isAuthenticated = true;
+								}
+							}
+						
+					}
+					System.out.println("Login Succesful");
+					
+				}catch (FileNotFoundException fe) {
+					
+					fe.printStackTrace();
+					
+				}
+		input.close();
+		return isAuthenticated;
+	   }
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("hi");
+		
+	}
+	
+	
+
+	}
+
+
